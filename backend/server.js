21c91193374e3c7a5876cfac2e10a7ec249036d9
Umesh Mane <umeshmane575@gmail.com>
2025-04-30 12:44:06 +0530

    chages in backend

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI , {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGO_URI)
 
 app.get('/tasks', async (req, res) => {
   const tasks = await Task.find();
